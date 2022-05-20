7e94bb08a9d44b51d8cdfbab21aea74a0e090000
Haꑭer <71404575+Andrew-programmer@users.noreply.github.com>
2022-05-20 13:27:17 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 $(function (){
+     
+    
+    
     if (screenWidth < 1024){
         addNewLinks();
     }
@@ -21,6 +24,26 @@ $(function (){
             $headerNav.css('display', 'flex');
         }
     })
+    
+    $("a").on('click', function(event) {
+    // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+    if (this.hash !== "") {
+      // Запретить поведение щелчка якоря по умолчанию
+      event.preventDefault();
+
+      // Хранить хэш
+      var hash = this.hash;
+
+      // Использование метода animate() jQuery для добавления плавной прокрутки страницы
+      // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+        window.location.hash = hash;
+    });
+    
 
     $body.keydown(function (event){
         if(event.keyCode === 27 && $searchField.hasClass('active')){
